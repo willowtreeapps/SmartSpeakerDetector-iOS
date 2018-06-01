a8acaf4205a4bca7e1313e20da95bef369bb0f87
Max Tkach <molotov2k@gmail.com>
2018-06-01 14:41:08 -0400

    adds logger functionality to SmartSpeakerDetector

--- Example/SmartSpeakerDetector/ViewController.swift
+++ Example/SmartSpeakerDetector/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController, SmartSpeakerDetectorDemoing {
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
+        detector.logger = self
         let font = UIFont(name: "Karla-Bold", size: 22.0)!
         self.navigationController?.navigationBar.titleTextAttributes = [.font: font, .foregroundColor: UIColor.white]
         self.navigationController?.navigationBar.tintColor = .white
@@ -46,7 +47,7 @@ class ViewController: UIViewController, SmartSpeakerDetectorDemoing {
     }
 }
 
-extension UIViewController: UICollectionViewDataSource {
+extension ViewController: UICollectionViewDataSource {
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as! ProductCollectionViewCell
@@ -58,5 +59,11 @@ extension UIViewController: UICollectionViewDataSource {
     
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return products.count
+    }
+}
+
+extension ViewController: SmartSpeakerDetectorLogger {
+    func log(event: String) {
+        print(event)
     }
 }
